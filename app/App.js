d882f14bc823fed417dcd66ebe4f66cacb4d9567
Monte9 <Monte9@users.noreply.github.com>
2017-02-13 20:46:54 -0800

    UI setup

--- app/App.js
+++ app/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import styles from './App.css';
 
+import _ from 'lodash';
+
+import ReactAudioPlayer from 'react-audio-player';
+import { Button, Grid, Row, Col, Jumbotron } from 'react-bootstrap';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {test: 'foo'};
   }
+
+  handleClick() {
+    console.log("Letter pressed");
+
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
   render() {
+    const alphabet = 'ABCDEFGHIJKLMNOPRSTUVWXYZ'.split('');
+
     return (
       <div className={styles.app}>
-        bar
+        <Jumbotron className={styles.jumboStyle}>
+          <h1 className={styles.jumboTitle}>Morse Code Player</h1>
+          <p>This is a simple web app to play morse code for sentence and numbers.</p>
+          <Grid>
+            <Row className="show-grid">
+              { alphabet.map(function(letter){
+                return (
+                  <Col sm={3} md={3}>
+                    <p><Button bsStyle="primary" bsSize="large" onClick={(e) => this.handleClick(e)}>{letter}</Button></p>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Grid>
+        </Jumbotron>
       </div>
     );
   }
